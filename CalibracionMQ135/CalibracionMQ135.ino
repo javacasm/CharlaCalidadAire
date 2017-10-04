582bd8136843cec1c258e5f7ae25f413e3bae049
javacasm <javacasm@gmail.com>
2017-10-04 09:40:56 +0200

    save date

--- CalibracionMQ135/CalibracionMQ135.ino
+++ CalibracionMQ135/CalibracionMQ135.ino
@@ -1,10 +1,17 @@
 #include "MQ135.h"
+#include <Time.h>
+
 void setup (){
-Serial.begin (9600);
+  Serial.begin (9600);
+  setTime(9, 24, 0, 4, 10, 2017);
 }
 void loop() {
-MQ135 gasSensor = MQ135(A0); // Attach sensor to pin A0
-float rzero = gasSensor.getRZero();
-Serial.println (rzero);
-delay(1000);
+  MQ135 gasSensor = MQ135(A0); // Attach sensor to pin A0
+  float rzero = gasSensor.getRZero();
+  char buffer[20];
+  sprintf(buffer,"%02d/%02d/%02d %02d:%02d:%02d,",day(),month(),year(),hour(),minute(),second());
+
+  Serial.print(buffer);
+  Serial.println (rzero);
+  delay(1000);
 }
